d97ac652835b004fa2a4c5c3ea60d5f9fa93be9e
APPLE\Apple <wogh8559@gmail.com>
2018-04-20 16:58:52 +0900

    오늘 한거 , reset 파싱해서 Reset 명령 날리기, App key Setting, 데이터 전송, 데이터 수신,

--- LoraTest_v1/LoraTest_v1.ino
+++ LoraTest_v1/LoraTest_v1.ino
@@ -1,24 +1,45 @@
 #include <SoftwareSerial.h>
  
-SoftwareSerial mySerial(2, 3); // RX, TX
+SoftwareSerial mySerial(2,3); // RX, TX
+unsigned short arr[10] = {0};
  
 void setup()  
 {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(19200);
   Serial.println("Goodnight moon!");
  
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(115200);
+  mySerial.begin(19200);
+  arr[0] = 0x4C; //L
+  arr[1] = 0x52; //R
+  arr[2] = 0x57; //W
+  arr[3] = 0x20; //space
+  arr[4] = 0x37; //4
+  arr[5] = 0x30; //C
+  
+  arr[6] = 0x20;
+  arr[7] = 0x0D;
+  arr[8] = 0x0A;
 }
  
 void loop() // run over and over
 {
   // Receive from bluetooth and send it to PC
-  if (mySerial.available())
+  if (mySerial.available()) {
     Serial.write(mySerial.read());
- 
-  // Receive from PC and send it to bluetooth
-  if (Serial.available())
-    mySerial.write(Serial.read());
+  }
+
+  iif(Serial.read() == 'a')
+      test(); 
 }
+
+
+void test(){
+  // Receive from PC and send it to bluetooth
+  for(int i = 0 ; i < 9 ; i++ ) {
+    mySerial.write(arr[i]);
+  }
+  delay(1000);
+}
+
